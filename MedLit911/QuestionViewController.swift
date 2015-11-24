809d7bd50af94e999ccdf6048351bf4617892a17
Jonathan Huang <jon.hc.huang@gmail.com>
2015-11-23 21:07:25 -0500

    Adding questions + iteration

--- MedLit911/QuestionViewController.swift
+++ MedLit911/QuestionViewController.swift
@@ -12,10 +12,23 @@ class QuestionViewController: UIViewController {
 
     @IBOutlet weak var questionLabel: UILabel!
     
+    var questionList = [
+        "Is the patient responsive to touch or speech?",
+        "Is the patient having discomfort in the center of the chest (>a few minutes or intermittent)? (uncomfortable pressure, squeezing, fullness or pain.)",
+        "Is the patient having trouble breathing or irregularly gasping for air?",
+        "Does one side of the patient's face droop or is it numb? (Ask the person to smile.)",
+        "Is one of the patient's arm weak or numb? Does one arm drift downward when the person raises both arms?",
+        "Is the patient's speech slurred -- are they unable to speak or hard to understand?",
+        "Is the patient having trouble breathing or is irregularly gasping for air?",
+        "Is the patient unconscious?"
+    ]
+    
+    var nextQuestion = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Set initial question : Jon
+        questionLabel.text = questionList[nextQuestion++]
     }
     
     @IBAction func onYesButtonPressed(sender: AnyObject) {
@@ -23,7 +36,10 @@ class QuestionViewController: UIViewController {
     }
     
     @IBAction func onNoButtonPressed(sender: AnyObject) {
-        // Go to next question : Jon
-        questionLabel.text = "Hello idiots"
+        if nextQuestion == questionList.count {
+            // Go to don't know view controller
+        } else {
+            questionLabel.text = questionList[nextQuestion++]
+        }
     }
 }
